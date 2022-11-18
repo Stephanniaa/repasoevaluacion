ebfd223437804fb53063d7cf5afdd0ffdb2890bb
Stephannia Garcia <dsgarcia815@misena.edu.co>
2022-11-17 21:05:39 -0500

    los otros

--- controllers/ventas.js
+++ controllers/ventas.js
@@ -22,7 +22,37 @@ const postVentas = async(req, res) =>{
     })
 }
 
+//DELETE
+const deleteVentas = async(req, res) =>{
+    const {nombreCliente} = req.query
+    const eliminarVentas = await modelventas.findOneAndDelete({nombreCliente:nombreCliente})
+    res.json({
+        msg: "DELETE API VENTAS",
+        eliminarVentas
+    })
+}
+
+//PUT
+const putVentas = async(req, res) =>{
+    const {nombreCliente, nombreProducto, total, nombreNuevo} = req.body
+    const modificarVentas = await modelventas.findOneAndUpdate({nombreCliente:nombreCliente},{nombreCliente:nombreNuevo,nombreProducto:nombreProducto, total:total})
+    res.json({
+        msg: "PUT API VENTAS",
+        modificarVentas
+    })  
+}
+
+//PATCH modifica cosas chiquitas
+const patchVentas = async(req,res) =>{
+    const {nombreCliente, nombreProducto} = req.body
+    const patch = await modelventas.findOneAndUpdate({nombreCliente:nombreCliente},{nombreProducto:nombreProducto})
+    res.json({
+        msg: "PATCH API VENTAS",
+        patch
+    })
+}
+
 
 module.exports = {
-    getVentas, postVentas
+    getVentas, postVentas, deleteVentas, putVentas, patchVentas
 }
--- routes/ventas.js
+++ routes/ventas.js
@@ -1,8 +1,11 @@
 const {Router} = require("express")
 const router = Router()
 
-const {getVentas, postVentas} = require("../controllers/ventas") //llamando lo que se esta exportando
+const {getVentas, postVentas, deleteVentas, putVentas, patchVentas} = require("../controllers/ventas") //llamando lo que se esta exportando
 router.get("/",getVentas)
 router.post("/",postVentas)
+router.delete("/",deleteVentas)
+router.put("/",putVentas)
+router.patch("/",patchVentas)
 
 module.exports = router
