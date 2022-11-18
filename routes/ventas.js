ab7e1d36335c54b0ca94f120f34d8014e008cc45
Stephannia Garcia <dsgarcia815@misena.edu.co>
2022-11-17 20:24:06 -0500

    lo otro que faltaba

--- routes/ventas.js
+++ routes/ventas.js
@@ -4,3 +4,5 @@ const router = Router()
 const {getVentas, postVentas} = require("../controllers/ventas") //llamando lo que se esta exportando
 router.get("/",getVentas)
 router.post("/",postVentas)
+
+module.exports = router
